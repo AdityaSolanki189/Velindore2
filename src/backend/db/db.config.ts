c46c076738ac7e43111115ae7067a2ec646c5d38
TheSaurojit <saurojitkarmakar947@gmail.com>
2025-07-10 14:56:10 +0530

    db

--- src/backend/db/db.config.ts
+++ src/backend/db/db.config.ts
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 
-const connection = await mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-export const db = drizzle(connection);
+export const db = drizzle(pool);
